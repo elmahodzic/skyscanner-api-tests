245f15d232d3c07b7ea0c232948fea1f316d5468
Elma Hodzic <elmahodzic612@gmail.com>
2021-02-26 10:22:20 +0100

    Update postman collection and smoke test

--- JavaRestAssured/SmokeTest/src/test/java/getQoutes.java
+++ JavaRestAssured/SmokeTest/src/test/java/getQoutes.java
@@ -1,7 +1,15 @@
 import org.testng.annotations.Test;
+
+import groovyjarjarasm.asm.TypeReference;
+
 import static io.restassured.RestAssured.*;
+
+import io.restassured.mapper.ObjectMapper;
 import io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import java.io.IOException;
+import java.util.List;
 
 
 public class getQoutes {
@@ -10,16 +18,14 @@ public class getQoutes {
 	public void skyscannerQuotes() {
 		
 		given().
-		header("apiKey","     ").
-		header("x-rapidapi-key", "   ").
-		header("x-rapidapi-host","   ").
+		header("apiKey","dummyapiKey").
+		header("x-rapidapi-key", "dummyxrapidapikey").
+		header("x-rapidapi-host","dummyxrapidapihost").
 		get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/BA/BAM/en-US/BELI-sky/BER-sky/2021-03-02").
 		then().	
 		statusCode(200).log().all();
 
 	}
 
-
-	}
-	
+}
 
